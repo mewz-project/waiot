4ab7250dcc71aa50cb058e467f5b60125f0d203e
ainozaki <aiai10piano@gmail.com>
2026-01-27 18:04:23 +0900

    update example

--- src/embedded-hal-wasm/examples/log.rs
+++ src/embedded-hal-wasm/examples/log.rs
@@ -3,29 +3,18 @@
 
 use panic_halt as _;
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin as _;
 use embedded_hal_wasm::delay::WasmDelay;
-use embedded_hal_wasm::digital;
 
 use embedded_hal_wasm::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> () {
-    let pin_number = 27;
-    
     println!("Logging from embedded-hal-wasm!");
     println!("We can log to the console using println! macro.");
-    println!("Now blinking GPIO pin {}...", pin_number);
     
-    let mut pin = digital::WasmGpioPin::new(pin_number).into_output();
     let mut delay = WasmDelay;
-    let duration_ns = 300_000_000;
-
-    for _ in 0..5 {
-        let _ = pin.set_high();
-        delay.delay_ns(duration_ns);
-        let _ = pin.set_low();
-        delay.delay_ns(duration_ns);
+    for i in 0..5 {
+        println!("Count: {}", i);
+        delay.delay_ns(1_000_000_000); // 1 second
     }
-    let _ = pin.set_low();
 }
