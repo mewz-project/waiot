095615e0a07faa98af47badb85d4135263acb2f0
ainozaki <aiai10piano@gmail.com>
2026-06-22 08:48:24 +0900

    Run *wasm from static file

--- src/platforms/freertos/stm32u575/NonSecure/Core/Src/stm_waiot_entry.c
+++ src/platforms/freertos/stm32u575/NonSecure/Core/Src/stm_waiot_entry.c
@@ -1,8 +1,8 @@
 #include "stm_waiot_entry.h"
 
+#include "embedded_wasm.h"
 #include "platform_wamr.h"
 #include "wamr.h"
-#include "wasm_test_module.h"
 
 #include <stdio.h>
 
@@ -13,7 +13,7 @@ void stm_waiot_start(void)
   waiot_platform_configure_wamr();
   init_wamr();
 
-  int ret = wamr_set_wasm_binary(g_wasm_test_module, g_wasm_test_module_size);
+  int ret = wamr_set_wasm_binary(g_waiot_embedded_wasm, g_waiot_embedded_wasm_size);
   if (ret != 0)
   {
     printf("waiot: failed to set wasm binary: %d\r\n", ret);
